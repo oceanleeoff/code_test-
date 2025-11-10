1e59994885f3affed90c4a907c01059fa37a10f0
oceanleeoff <srtu1586@nate.com>
2025-11-10 18:42:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.29 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -2,14 +2,14 @@ def solution(array):
     num_list = set(array)
     freq = []
     
-    # 빈도값 추가
+    # 빈도 수 추가
     for i in num_list :
         freq.append(array.count(i))
     
     # 최대 빈도 수
     max_freq = max(freq)
     
-    # 최빈값이 여러 개인 경우
+    # 최대 빈도 수의 동일 값이 여러 개인 경우
     if freq.count(max_freq) > 1:
         return -1
     
