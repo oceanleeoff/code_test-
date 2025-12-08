273931cb6e4de75c17a2bcf102ace20399c524d4
oceanleeoff <srtu1586@nate.com>
2025-12-08 18:15:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.45 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,27 @@
+def solution(s):
+    num_dict = {
+        'zero': '0',
+    	'one' : '1',
+    	'two' : '2',
+    	'three' : '3',
+    	'four' : '4',
+    	'five' : '5',
+    	'six' : '6',
+    	'seven' : '7',
+    	'eight' : '8',
+    	'nine' : '9'
+    }
+    
+    answer = ''
+    temp = ''
+    
+    for i in s :
+        if i.isdigit() :
+            answer += i
+        else :
+            temp += i
+            if temp in num_dict :
+                answer += num_dict[temp]
+                temp = ''
+      
+    return int(answer)
